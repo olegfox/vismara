23c391e99990f5122dd962cd5e4be9aa790343a3
olegfox <info@olegfox.com>
2015-06-15 15:09:45 +0300

    fix labguage catalogs area

--- src/Site/MainBundle/Entity/Client.php
+++ src/Site/MainBundle/Entity/Client.php
@@ -21,7 +21,7 @@ class Client implements UserInterface, \Serializable
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=25, unique=true)
+     * @ORM\Column(type="string", length=25, unique=false)
      */
     private $username;
 
